70f1901e86091ef07885ff75900eeeb8b5a007ac
chrischall <chris.c.hall@gmail.com>
2026-08-28 16:42:05 -0400

    chore(main): release 0.3.1

--- src/version.ts
+++ src/version.ts
@@ -4,4 +4,4 @@
  * sync from here (see `release-please-config.json` extra-files) or from this
  * constant, so nothing is hand-bumped.
  */
-export const VERSION = '0.3.0'; // x-release-please-version
+export const VERSION = '0.3.1'; // x-release-please-version
